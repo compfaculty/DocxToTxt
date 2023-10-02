e6e52ffbbdd1516c74888766fe3d8792ed4a3ebb
Alex Granovsky <compfaculty@users.noreply.github.com>
2023-10-02 12:38:09 +0300

    init

---
+++ DocxToTxt/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+var convertor = new DocxToTxt();
+try
+{
+    convertor.Run(args[0], null);
+}
+catch (Exception)
+{
+    Console.WriteLine("Error: looks docx file is missing or not found, i.e. DocxToTxt.exe \"d:\\myfile.docx\"");
+}
+
+public sealed class DocxToTxt
+{
+    private void SaveToTxt(string? content, string savePath)
+    {
+        File.WriteAllText(savePath, content);
+    }
+
+    private void ConvertDocxToTxt(string docxPath, string txtPath)
+    {
+        string content = ReadFromDocxText(docxPath);
+        SaveToTxt(content, txtPath);
+    }
+
+    public string ReadFromDocxText(string filePath)
+    {
+        StringBuilder documentText = new StringBuilder();
+
+        using (var doc = WordprocessingDocument.Open(filePath, false))
+        {
+            Body body = doc.MainDocumentPart!.Document.Body ?? throw new Exception("doc failed");
+
+            foreach (var para in body.Elements())
+            {
+                documentText.AppendLine(para.InnerText);
+            }
+        }
+
+        return documentText.ToString();
+    }
+
+
+    public void Run(string docxPath, string? txtPath)
+    {
+        txtPath ??= docxPath + ".txt";
+        ConvertDocxToTxt(docxPath, txtPath);
+        Console.WriteLine($"Conversion completed! Check \"{txtPath}\"");
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple docx to txt convertor
+
+# Usage 
+
+ DocxToTxt.exe "d:\\my file.docx"
+ Output: d:\\my file.docx.txt
